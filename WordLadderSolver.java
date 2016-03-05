31a6ed635791e920504ebf4433d2ab67f9b3089b
sbordov <stefan.bordovsky@gmail.com>
2016-03-05 01:46:54 -0600

    Got rid of need to overwrite MakeLadder()! More abstraction on some of the internal MakeLadder() methods would be nice, but whatever. At least stuff works.

--- WordLadderSolver.java
+++ WordLadderSolver.java
@@ -26,7 +26,10 @@ public class WordLadderSolver implements Assignment4Interface
     @Override
     public List<String> computeLadder(String startWord, String endWord) throws NoSuchLadderException 
     {
-    	List <String> result = makeLadder(startWord, endWord, 0);
+        List<String> solutions = new ArrayList<String>();
+        solutions.add(startWord);
+        addSolution(startWord);
+    	List <String> result = makeLadder(startWord, endWord, 0, solutions);
     	clearSolutions();
     	return result;
         //throw new UnsupportedOperationException("Not implemented yet!");
@@ -39,67 +42,6 @@ public class WordLadderSolver implements Assignment4Interface
     }
 
     // add additional methods here
-    public List<String> makeLadder(String startWord, String endWord, int changeIndex){
-    	List<String> result = new ArrayList<String>();
-    	if(startWord.equals(endWord)){		// start word = end word
-    		result.add(startWord);
-    		result.add(endWord);
-    		return result;
-    	}
-        if(numDifferentChars(startWord, endWord) == 1){
-        	result.add(startWord);
-        	result.add(endWord);
-        	return result;
-        }
-        
-        String letters = "abcdefghijklmnopqrstuvwxyz";
-        List<String> possibles = new ArrayList<String>();
-        List<String> solutions = new ArrayList<String>();
-        for(int x = 0; x < 5; x++){
-        	if(x == changeIndex){
-        		x++;
-        	}
-        	char check = startWord.charAt(x);
-        	for(int y = 0; y < letters.length(); y++){
-        		if(check != letters.charAt(y)){
-        			int loop = 0;
-        			String test = "";
-        			while(loop != x){
-        				test += startWord.charAt(loop);
-        				loop++;
-        			}
-        			test += letters.charAt(y);
-        			loop++;
-        			while(loop < 5){
-        				test += startWord.charAt(loop);
-        				loop++;
-        			}
-        			if(isInDictionary(test)){
-        				test = Integer.toString(numDifferentChars(test, endWord)) + Integer.toString(x) + test;
-        				int index = binarySearch(possibles, test);
-        				possibles.add(index, test);
-        			}
-        		}
-        	}
-        }
-        Iterator<String> it = possibles.iterator();
-        boolean route = false;
-        while(it.hasNext()){
-        	String temp = it.next();
-        	int change = Integer.parseInt(temp.substring(1, 2));
-        	temp = temp.substring(2);
-        	solutions.add(temp);
-        	addSolution(temp);
-        	result = makeLadder(temp, endWord, change, solutions);
-        	if(result != null){
-        		route = true;
-        		result.add(0, startWord);
-        		return result;
-        	}
-        }
-        return null;
-    }
-    
     public List<String> makeLadder(String startWord, String endWord, int changeIndex, List<String> solutions){
     	List<String> result = new ArrayList<String>();
     	if(startWord.equals(endWord)){		// start word = end word
